82888bc46b32ad959e344697f061a5ab9b40b8b0
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-11-29 16:56:13 -0300

    Desafio Tecnico 5
    Desafio Tecnico 5-POO

---
+++ Personagem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTecnico3
+{
+    internal class Personagem
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        List<string> caracteristicas = new List<string>();
+
+        public Personagem(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+        public void adicionarCaracteristicas(string carac)
+        {
+            caracteristicas.Add(carac);
+        }
+        public void mostrarDados()
+        {
+            Console.WriteLine($"\nNome do personagem: {nome}");
+            Console.WriteLine($"Idade do personagem: {idade}");
+            foreach(string carac in caracteristicas)
+            {
+                Console.WriteLine($"Características: {carac}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTecnico3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem p1 = new Personagem("Inej Ghafa", 20);
+            Personagem p2 = new Personagem("JJ Maybank", 18);
+
+            Console.WriteLine("\nDigite uma característica para a personagem 1: ");
+            p1.adicionarCaracteristicas(Console.ReadLine());
+
+            Console.WriteLine("\nDigite 1 caso você queira adicionar mais características para a personagem e outro número caso você não queira: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            if(numero1 == 1)
+            {
+                Console.WriteLine("\nDigite uma característica: ");
+                p1.adicionarCaracteristicas(Console.ReadLine());
+                p1.mostrarDados();
+            }
+            else
+            {
+                p1.mostrarDados();
+            }
+
+            Console.WriteLine("\nDigite uma característica para o personagem 2: ");
+            p2.adicionarCaracteristicas(Console.ReadLine());
+
+            Console.WriteLine("\nDigite 1 caso você queira adicionar mais características para o personagem e outro número caso você não queira: ");
+            int numero2 = int.Parse(Console.ReadLine());
+            if (numero2 == 1)
+            {
+                Console.WriteLine("\nDigite uma característica: ");
+                p2.adicionarCaracteristicas(Console.ReadLine());
+                p2.mostrarDados();
+            }
+            else
+            {
+                p2.mostrarDados();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
